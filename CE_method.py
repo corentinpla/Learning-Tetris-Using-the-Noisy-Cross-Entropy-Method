f0520b0a25becc5b1c34c926f1a155ead8d1877d
Corentin Pla <103648354+corentinpla@users.noreply.github.com>
2023-04-20 11:16:38 +0200

    modif as paper

--- CE_method.py
+++ CE_method.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 
 
-def simulation_CE(alpha, N_mean, N_iteration,rho): #alpha : taux d'actualistion 
-                               #N_mean: nombre de simulation par vecteur
+def simulation_CE(alpha, N_iteration,rho): #alpha : taux d'actualistion 
+                              
                                #N_iteration : nombre d'iterations
                                #rho : the fraction of verctors that are selected
                                #retourne L_plot : le score maximal par itération
@@ -32,30 +32,25 @@ def simulation_CE(alpha, N_mean, N_iteration,rho): #alpha : taux d'actualistion
         # Evaluate each parameter pool
         N = 100
         sample_list = []
-        sample_mean = []
+        sample_score = []
 
         for i in range(N):
             
             sample = distribution.rvs() #vecteur de paramètre W
-            score_mean=[] #liste qui va contenir la moyenne des scores pour W sur N_mean parties
+            
 
-            for i in range(N_mean):
-                score_mean.append(Tetris.simulation_without_graphic(sample))
-
+            sample_score.append(Tetris.simulation_without_graphic(sample))
             sample_list.append(sample)
-            sample_mean.append(np.mean(score_mean))
-            print(np.mean(score_mean))
             
 
         # Keeping the rho*N bests vectors
         k=math.floor(N*rho)
 
-        indices=sorted(range(len(sample_mean)), key=lambda i: sample_mean[i], reverse=True)[:k]
+        indices=sorted(range(len(sample_score)), key=lambda i: sample_score[i], reverse=True)[:k]
         sample_high = [sample_list[i] for i in indices]
-        print(sample_high)
+        best_sample=sample_list[indices[0]]
 
         # New parameter estimation using MLE
-
 
         mean = np.mean(sample_high, axis = 0)
         cov =  np.cov(sample_high, rowvar = False,bias=True)
@@ -66,17 +61,12 @@ def simulation_CE(alpha, N_mean, N_iteration,rho): #alpha : taux d'actualistion
         parameters.append((alpha * np.array(res[0]) + (1 - alpha) * np.array(parameters[-1][0]),
                         alpha ** 2 * np.array(res[1]) + (1 - alpha) ** 2 * np.array(parameters[-1][1])))    
 
-        L_plot.append(max(sample_mean))
-        plt.plot(L_plot)
-        plt.show()
+        for k in range (30):
+            L_mean.append(Tetris.simulation_without_graphic(best_sample))
+
+
+        L_plot.append(np.mean(L_mean))
         t+=1
     
     return(L_plot,(mean, cov))
-
-def get_mean_from_sample(sample,N_mean):
-    L_mean=[]
-    for k in range (N_mean):
-        L_mean.append(Tetris.simulation_without_graphic(sample))
-
-    return(np.mean(L_mean))
 
